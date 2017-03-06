3513cf11017874462aa7c9cd9a6f4cadc10e66e2
Andrew Cameron <andrew.cameron@protonmail.com>
2017-03-06 15:09:04 +0000

    A little cleaner

--- main.py
+++ main.py
@@ -1,45 +1,62 @@
 from datetime import datetime
 import time
 
-# from bson import BSON
-# from pymongo import MongoClient
+from pymongo import MongoClient
 import requests
 
 import config as conf
 
 
 class Logger:
-    """"""
+    """A simple logger"""
 
     def __init__(self, log_file):
         self.log_file = log_file
 
     def log(self, timestamp, message):
+        """writes timestamp and message to file
+        params  : timestamp (), message (str)
+        returns : -
+        """
         with open(self.log_file, "at") as f:
             f.write("{} : {}\n".format(timestamp, message))
 
 
-def fetch(url):
-    """"""
+def fetch_json(url):
+    """Returns a dictionary of JSON returned by a get request to a url
+    params  : url (str)
+    returns : [dict]
+    """
     try:
         r = requests.get(url)
     except requests.ConnectionError:
         logger.log(datetime.now, "Connection Error")
-    if r.json():
-        return dict(r.json())
+    return r.json()
 
 
-def store(dictionary):
-    """"""
-    print(dictionary)
+def store(data, database, collection):
+    """Saves a python dictionary object in a mongoDB collection
+    params  : data [dict], database (str), collection (str)
+    returns : pymongo.results.Insert_one_result
+    """
+    mc = MongoClient()[conf.DATABASE_NAME][conf.COLLECTION_NAME]
+    r = mc.insert_many(data)
+    return r
 
 
 def main():
-    data = fetch(conf.URL + conf.API_KEY,
-                 conf.DATABASE_NAME,
-                 conf.COLLECTION_NAME)
-
+    while True:
+        data = fetch_json(conf.URL + conf.API_KEY)
+        if not data:
+            logger.log(datetime.now(), "GET ERROR")
+        else:
+            id_ = store(data, conf.DATABASE_NAME, conf.COLLECTION_NAME)
+            if not id_:
+                logger.log(datetime.now(), "Error Writing {}".format(data))
+        time.sleep(60 * 5)
 
 if __name__ == "__main__":
+    if not conf.API_KEY or not conf.COLLECTION_NAME or not conf.DATABASE_NAME or not conf.LOG_FILE or not conf.URL:
+        raise SystemExit("Invalid Config")
     logger = Logger(conf.LOG_FILE)
     main()

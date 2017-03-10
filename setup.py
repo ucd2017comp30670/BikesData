3c89beb09f9b54c52edfee1b94714d6306e5c9a2
Andrew Cameron <andrew.cameron@protonmail.com>
2017-03-10 11:29:14 +0000

    created setup file, created src dir

---
+++ setup.py
@@ -0,0 +1,19 @@
+"""
+The setup for installation of the BikesData package.
+Helpful resource:
+https://docs.python.org/3.6/distutils/setupscript.html
+"""
+
+from distutils.core import setup
+
+setup(
+    name="BikesData",
+    version=0.1,
+    description="Fetches and stores data about Dublin Bikes from the jcdecaux API",
+    author="Team 12",
+    keywords="Dublin bikes data getter",
+    packages=["BikesData"],
+    package_dir={"": "src"},
+    install_requires=open("requirements.txt", "rt").readlines(),
+    entry_points={'console_scripts': ['bikes-data = BikesData.main:main']}
+)
